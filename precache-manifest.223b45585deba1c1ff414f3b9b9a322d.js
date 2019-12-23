5e54115d457f3a5df3fb9a9c4c8e0cec7c809417
Randika Rodrigo <randika.rodrigo@qmatic.com>
2019-12-23 21:18:34 +0530

    Updates

---
+++ precache-manifest.223b45585deba1c1ff414f3b9b9a322d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67e872ef12249e203e646bbc4a77acd",
+    "url": "/people-index/index.html"
+  },
+  {
+    "revision": "4c49e2732f89326b91f9",
+    "url": "/people-index/static/css/main.1658423a.chunk.css"
+  },
+  {
+    "revision": "cc412fba226e48d396ae",
+    "url": "/people-index/static/js/2.e03d1add.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/people-index/static/js/2.e03d1add.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c49e2732f89326b91f9",
+    "url": "/people-index/static/js/main.a7abcd29.chunk.js"
+  },
+  {
+    "revision": "c1069654e2500b8feafb",
+    "url": "/people-index/static/js/runtime-main.4af2d635.js"
+  }
+]);
